7bd338c228fb8b4cfc7c463ec4964293b9554c04
aRafael <Rafaelcroes2006@gmail.com>
2024-02-28 13:03:36 +0100

    first commit

---
+++ slimapi2/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/api/metingen', function (Request $request, Response $response) {
+
+    $body = json_encode([1 => "One", 2 => "Two"]);
+
+    $response->getBody()->write($body);
+
+    return $response->withHeader('Content-Type', 'application/json');
+
+});
+
+$app->run();
