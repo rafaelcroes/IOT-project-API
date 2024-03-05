4e8c22dad99eba91f0ed1f786381432fdd501cb5
aRafael <Rafaelcroes2006@gmail.com>
2024-03-05 10:02:29 +0100

    Change API endpoint to retrieve sensor data from the database

--- slimapi2/public/index.php
+++ slimapi2/public/index.php
@@ -10,14 +10,23 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/api/metingen', function (Request $request, Response $response) {
+$app->get('/api/sensoren', function (Request $request, Response $response) {
 
-    $body = json_encode([1 => "One", 2 => "Two"]);
+    $dsn = "mysql:host=127.0.0.1;dbname=iot;charset=utf8";
+
+    $pdo =  new PDO($dsn, "RAUL", "123", [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $stmt = $pdo->query("SELECT * FROM sensor");
+
+    $data = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+    $body = json_encode($data);
 
     $response->getBody()->write($body);
-
-    return $response->withHeader('Content-Type', 'application/json');
-
+    
+    return $response->withHeader('Content-Type', 'application/json');  
 });
 
 $app->run();
