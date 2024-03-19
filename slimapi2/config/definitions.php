c5fababe00eac1aa1b6cae84718c64f8328fdf06
aRafael <Rafaelcroes2006@gmail.com>
2024-03-19 09:17:06 +0100

    made is secure

--- slimapi2/config/definitions.php
+++ slimapi2/config/definitions.php
@@ -4,10 +4,10 @@ use App\Database;
 
 return [
     Database::class => function(){
-        return new Database(host: '127.0.0.1',
-                            dbname:  'iot',
-                            user: 'RAUL',
-                            password: '123');
+        return new Database(host: 'ip_adress',
+                            dbname:  'idbname',
+                            user: 'username',
+                            password: 'password');
     }
 ];
 
